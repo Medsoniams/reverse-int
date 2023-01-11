7d93d5fdd5510cff948a7e67063097c0072348f6
Bohdan Kariev <karevbogdan@gmail.com>
2023-01-11 20:46:19 +0200

    feat: add index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+
+  let nToString = Math.abs(n).toString();
+  let result = "";
+  for (let i = 0; i <= nToString.length - 1; i += 1) {
+    result = nToString[i] + result;
+  }
+  return Number(result);
 }
